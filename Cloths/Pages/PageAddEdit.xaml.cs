32a49fc5786e4a245c111a20abf4611278f84a7f
10180023 <10180023@B316PC06>
2021-12-07 11:46:05 +0300

    Добавьте файлы проекта.

---
+++ Cloths/Pages/PageAddEdit.xaml.cs
@@ -0,0 +1,119 @@
+﻿using Cloths.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cloths.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddEdit.xaml
+    /// </summary>
+    public partial class PageAddEdit : Page
+    {
+        private Material currentMaterial = new Material();
+
+        public PageAddEdit(Material selectedMaterial)
+        {
+            InitializeComponent();
+
+            if (selectedMaterial != null)
+            {
+                currentMaterial = selectedMaterial;
+            }
+
+            DataContext = currentMaterial;
+
+            cbType.ItemsSource = ClothsEntities.GetContext().MaterialType.ToList();
+        }
+        /// <summary>
+        /// Проверка введенных данных, добавление или сохранение изменений записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(tbTitle.Text))
+                errors.AppendLine("Введите наименование");
+            if (int.Parse(tbInPack.Text.ToString()) <= 0)
+                errors.AppendLine("Введите кол-во в упаковке");
+            if (string.IsNullOrWhiteSpace(tbUnit.Text))
+                errors.AppendLine("Введите ед. измерения");
+            if (tbInStock.Text.ToString() == "0")
+                errors.AppendLine("Введите кол-во на складе");
+            if (tbMin.Text.ToString() == "0")
+                errors.AppendLine("Введите мин. кол-во");
+            if (tbCost.Text.ToString() == "0")
+                errors.AppendLine("Введите стоимость");
+            if (string.IsNullOrWhiteSpace(tbInStock.Text))
+                currentMaterial.Image = null;
+            if (cbType.SelectedItem == null)
+                errors.AppendLine("Выберите тип");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Внимание", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (currentMaterial.ID == 0)
+            {
+                currentMaterial.MaterialType =  (MaterialType)cbType.SelectedItem;
+                ClothsEntities.GetContext().Material.Add(currentMaterial);
+            }
+
+            try
+            {
+                ClothsEntities.GetContext().SaveChanges();
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        /// <summary>
+        /// Отмена ввода изменений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+        /// <summary>
+        /// Удаление выбранной записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы хотите удалить запись?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ClothsEntities.GetContext().Material.Remove(currentMaterial);
+                    ClothsEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Запись удалена", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Manager.MainFrame.GoBack();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Невозможно удалить не созданную запись", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+    }
+}
---
+++ Cloths/Pages/PageMaterials.xaml.cs
@@ -0,0 +1,179 @@
+﻿using Cloths.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cloths.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMaterials.xaml
+    /// </summary>
+    public partial class PageMaterials : Page
+    {
+        List<Material> materials = ClothsEntities.GetContext().Material.ToList();
+
+        public PageMaterials()
+        {
+            InitializeComponent();
+
+            var types = ClothsEntities.GetContext().MaterialType.ToList();
+            types.Insert(0, new MaterialType { 
+                Title = "Все типы"
+            });
+
+            List<string> sort = new List<string>();
+            sort.Add("Сортировка");
+            sort.Add("По наименованию");
+            sort.Add("По остаткам");
+            sort.Add("По стоимости");
+
+            cbFilter.ItemsSource = types;
+            cbFilter.DisplayMemberPath = "Title";
+            cbFilter.SelectedValue = "ID";
+            cbFilter.SelectedIndex = 0;
+
+            cbSort.ItemsSource = sort;
+            cbSort.SelectedIndex = 0;
+
+            tbFinder.Text = "Поиск по наименованию";
+
+            lvMaterials.ItemsSource = materials;
+        }
+        /// <summary>
+        /// Поиск, сортировка и фильтрация материалов
+        /// </summary>
+        private void UpdateMaterials()
+        {
+            materials = ClothsEntities.GetContext().Material.ToList();
+
+            if (cbSort.SelectedIndex > 0)
+            {
+                switch (cbSort.SelectedIndex)
+                {
+                    case 1:
+                        if (rbAsc.IsChecked == true)
+                        {
+                            materials = materials.OrderBy(m => m.Title).ToList();
+                        }
+                        else
+                        materials = materials.OrderByDescending(m => m.Title).ToList();
+                        break;
+                    case 2:
+                        if (rbAsc.IsChecked == true)
+                        {
+                            materials = materials.OrderBy(m => m.CountInStock).ToList();
+                        }
+                        else
+                        materials = materials.OrderByDescending(m => m.CountInStock).ToList();
+                        break;
+                    case 3:
+                        if (rbAsc.IsChecked == true)
+                        {
+                            materials = materials.OrderBy(m => m.Cost).ToList();
+                        }
+                        else
+                        materials = materials.OrderByDescending(m => m.Cost).ToList();
+                        break;
+                }
+            }
+
+            if (tbFinder.Text != "Поиск по наименованию")
+            {
+                materials = ClothsEntities.GetContext().Material.ToList();
+                materials = materials.Where(m => m.Title.ToLower().Contains(tbFinder.Text.ToLower())).ToList();
+            }    
+            
+            if (cbFilter.SelectedIndex > 0)
+            {
+                materials = materials.Where(m => m.MaterialType.Equals(cbFilter.SelectedValue)).ToList();
+            }
+
+            lvMaterials.ItemsSource = materials;
+        }
+        /// <summary>
+        /// Открытие окна добавления материалов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new PageAddEdit(null));
+        }
+        /// <summary>
+        /// открытие окна для редактирования записи
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new PageAddEdit((sender as Button).DataContext as Material));
+        }
+        /// <summary>
+        /// Обновление списка материалов
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ClothsEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(m => m.Reload());
+            materials = ClothsEntities.GetContext().Material.ToList();
+            lvMaterials.ItemsSource = materials;
+        }
+        /// <summary>
+        /// Сброс поля поиска
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbFinder_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbFinder.Text = "";
+        }
+        /// <summary>
+        /// Поиск по наименованию
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbFinder_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateMaterials();
+        }
+        /// <summary>
+        /// Сортировка
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateMaterials();
+        }
+        /// <summary>
+        /// Фильтрация
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateMaterials();
+        }
+        /// <summary>
+        /// Переключение режима сортировки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void radioButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateMaterials();
+        }
+    }
+}
